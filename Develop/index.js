ee3a2becc34206af5ac330bee61afba8c7c239c9
Brooke <brookedunlap97@gmail.com>
2024-05-02 23:14:46 -0500

    Installed inquirer and added code to index.js to prompt for user inputs to a list of questions upon initialization. The data from the user's inputs get passed into the writeToFile() once completed.

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,81 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//include table of contents
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide installation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information:'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'List your collaborators:'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide test instructions:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your application:',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'None']
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Enter your GitHub username:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:'
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'idk:'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (error) =>
+    error ? console.error(error) : console.log('Success!')
+    );  
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then((data) => {
+            const markdown = generateMarkdown(data);
+            writeToFile('README.md', data);
+            console.log("Congrats! You have successfully created your README!!");
+        }
+    );
+}
 
 // Function call to initialize app
 init();
