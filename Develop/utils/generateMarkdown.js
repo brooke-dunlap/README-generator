489862aeb72b2b63e8bdc153ed2f5d2acb3ddbdf
Brooke <brookedunlap97@gmail.com>
2024-05-05 15:58:23 -0500

    Fixed code in renderLicenseBadge()

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+  if (license === 'MIT'){
     return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
   } else if (license === 'GNU GPLv3') {
       return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
@@ -8,6 +9,7 @@ function renderLicenseBadge(license) {
       return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
   } else {
       return '';
+  }
 }
 
 // TODO: Create a function that returns the license link
